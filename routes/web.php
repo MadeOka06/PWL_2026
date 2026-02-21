dc55a0a44380fe26b09f7a6f2360fc4429536ea4
MadeOka06 <okadarmestha@gmail.com>
2026-02-21 13:40:25 +0700

    P2_Praktikum 1

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,28 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+Route::get('/world', function() {
+    return 'World';
+});
+Route::get('/Selamat Datang', function() {
+    return 'Selamat Datang';
+});
+Route::get('/About', function() {
+    return 'NIM : 244107020177 
+    Nama : I Gusti Agung Made Oka Darmestha';
+});
+Route::get('/user/{name}', function ($name){
+    return 'Nama saya '.$name;
+});
+Route::get('/posts/{post}/comments/{comment}', function ($postId,$commentId){
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+Route::get('/articles/{id}', function ($articlesId){
+    return 'Halaman Artikel dengan '." ID : ".$articlesId;
+});
+Route::get('user/{name?}',function ($name='John') {
+    return 'Nama saya '.$name;
 });
